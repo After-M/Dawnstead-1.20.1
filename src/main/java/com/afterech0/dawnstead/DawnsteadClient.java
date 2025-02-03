62497eb08aef48b1d358e4c0448c83c82e5f88a5
AfterEHC0 <1511696142@qq.com>
2025-02-04 00:14:27 +0800

    Initial commit

---
+++ src/main/java/com/afterech0/dawnstead/DawnsteadClient.java
@@ -0,0 +1,10 @@
+package com.afterech0.dawnstead;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class DawnsteadClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+        
+    }
+}
